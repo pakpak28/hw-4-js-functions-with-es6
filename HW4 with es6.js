48a57fd505c8d2d0e79822c9e9433a0e1359662e
pakpak28 <evgenymrpakin@gmail.com>
2020-10-29 14:59:06 +0300

    fixes

--- HW4 with es6.js
+++ HW4 with es6.js
@@ -4,7 +4,7 @@
 const splitAndMerge = (str, sp) =>
   str
     .split(" ")
-    .map((w) => w.split("").join(sp))
+    .map((word) => word.split("").join(sp))
     .join(" ");
 
 console.log(splitAndMerge("My name is John", " ")); // "M y n a m e i s J o h n"
@@ -16,14 +16,17 @@ let obj = { name: "Jeremy", age: 24, role: "Software Engineer" };
 
 console.log(convert(obj)); // [["name", "Jeremy"], ["age", 24], ["role", "Software Engineer"]]
 // 3) Complete the method/function  for Capitalize
-function toCamelCase(str) {
-  return str.replace(/[-_](.)/g, (_, char) => char.toUpperCase());
-}
+const toCamelCase = (str) =>
+  str.replace(/[-_](.)/g, (_, char) => char.toUpperCase());
 
 console.log(toCamelCase("the-stealth-warrior")); // returns "theStealthWarrior"
 console.log(toCamelCase("the-stealth-warrior")); // returns "TheStealthWarrior"
 // 4) Write a reverse function
-const reverseStr = (str) => [...str].reverse().join("");
+const reverseStr = (str) =>
+  str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
 
 console.log(reverseStr("A fun little challenge!")); // "A nuf elttil !egnellahc"
 // 5) Write a function `stringExpansion`
@@ -35,8 +38,8 @@ console.log(stringExpansion("3d332f2a")); // "dddffaa"
 console.log(stringExpansion("abcde")); // "abcde"
 
 // 6) Write `largest` and `smallest` functions
-const largest = (...n) => Math.max(...n);
-const smallest = (...n) => Math.min(...n);
+const largest = (...arg) => Math.max(...arg);
+const smallest = (...arg) => Math.min(...arg);
 
 console.log(largest(2, 0.1, -5, 100, 3)); // 100
 console.log(smallest(2, 0.1, -5, 100, 3)); // -5
@@ -52,6 +55,8 @@ console.log(newArray[4]()); // should return 50`
 
 // 8) Write function `sum`
 const sum = (...n) => (n.length == 0 ? 0 : n[0] + sum(...n.slice(1)));
+//solution for 'reduce'method
+const sum = (...numbers) => numbers.reduce((a, b) => a + b);
 
 console.log(sum(1, 3, 5, 7)); // 16
 // 9) Write function `countDown`
